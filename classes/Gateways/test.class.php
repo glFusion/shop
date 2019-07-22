b7d28296afe09a12581f908ec518be83c7374b0e
Lee Garner <lee@leegarner.com>
2019-07-22 09:52:38 -0700

    Set test gateway to not require IPN

--- classes/Gateways/test.class.php
+++ classes/Gateways/test.class.php
@@ -253,6 +253,18 @@ class test extends \Shop\Gateway
         return $retval;
     }
 
+
+    /**
+     * Check if this gateway allows an order to be processed without an IPN msg.
+     * The Check gateway does allow this as it just presents a remittance form.
+     *
+     * @return  boolean     True
+     */
+    public function allowNoIPN()
+    {
+        return true;
+    }
+
 }
 
 ?>
