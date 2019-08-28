11ddf152a4c6c0e42c9c1b983ea9c8b1d2cd1de0
Lee Garner <lee@leegarner.com>
2019-08-28 09:25:31 -0700

    Add new sitemap driver, fix url creation and access check

---
+++ classes/Sitemap/shop.class.php
@@ -0,0 +1,122 @@
+<?php
+/**
+ * Sitemap driver for the Shop plugin.
+ *
+ * @author      Lee Garner <lee@leegarner.com>
+ * @copyright   Copyright (c) 2017-2019 Lee Garner
+ * @package     shop
+ * @version     v1.0.0
+ * @since       v0.7.0
+ * @license     http://opensource.org/licenses/gpl-2.0.php
+ *              GNU Public License v2 or later
+ * @filesource
+ */
+
+/**
+ * Sitemap driver class for Shop.
+ * @paackage shop
+ */
+class sitemap_shop extends sitemap_base
+{
+    /** Plugin name.
+     * @var string */
+    protected $name = 'shop';
+
+    /**
+     * Get the plugin main URL.
+     *
+     * @return  string  Entry URL
+     */
+    public function getEntryPoint()
+    {
+        global $_SHOP_CONF;
+        return $_SHOP_CONF['shop_enabled'] ? SHOP_URL : '';
+    }
+
+
+    /**
+     * Get the plugin display name.
+     *
+     * @return  string  Display name
+     */
+    public function getDisplayName()
+    {
+        global $LANG_SHOP, $_SHOP_CONF;
+        return $_SHOP_CONF['shop_enabled'] ? $LANG_SHOP['main_title'] : '';
+    }
+
+
+    /**
+     * Get items for the sitemap under a specific category.
+     *
+     * @param   integer $cat_id     Category ID
+     * @return  array       Array of sitemap entries
+     */
+    public function getItems($cat_id = 0)
+    {
+        global $_TABLES, $_USER, $_SHOP_CONF;
+
+        $entries = array();
+        if (!$_SHOP_CONF['shop_enabled']) {
+            return $entries;
+        }
+        $opts = array(
+            'groups' => $this->groups,
+        );
+        if ($cat_id > 0) {
+            $opts['cat_id'] = $cat_id;
+        }
+        $items = PLG_getItemInfo('shop', '*', 'id,introtext,date,url', $this->uid, $opts);
+        if (is_array($items)) {
+            foreach ($items as $A) {
+                $entries[] = array(
+                    'id'    => $A['id'],
+                    'title' => $A['introtext'],
+                    'uri'   => $A['url'],
+                    'date'  => $A['date'],
+                    'image_uri' => false,
+                );
+            }
+        }
+        return $entries;
+    }
+
+
+    /**
+     * Get the immediate child categories under a given base category.
+     *
+     * @param   integer $base   Category ID
+     * @return  array       Array of categories
+     */
+    public function getChildCategories($base = false)
+    {
+        global $_SHOP_CONF;
+
+        $retval = array();
+        if (!$_SHOP_CONF['shop_enabled']) {
+            return $retval;
+        }
+
+        if ($base === false) {
+            $Root = Shop\Category::getRoot();
+        } else {
+            $Root = Shop\Category::getInstance((int)$base);
+        }
+        $cats = $Root->getChildren();
+        foreach ($cats as $Cat) {
+            if ($Cat->hasAccess($this->groups)) {
+                $retval[] = array(
+                    'id'        => $Cat->cat_id,
+                    'title'     => $Cat->cat_name,
+                    'uri'       => SHOP_URL . '/index.php?category=' . $A['cat_id'],
+                    'date'      => false,
+                    'image_uri' => $Cat->ImageUrl(),
+                );
+            }
+        }
+        return $retval;
+    }
+
+}
+
+?>
