b4b346996c03683c894b1c3c4080dbac4a2ea2d9
Lee Garner <lee@leegarner.com>
2020-02-17 16:22:18 -0800

    Add tax report

--- language/english_utf-8.php
+++ language/english_utf-8.php
@@ -85,6 +85,7 @@ $LANG_SHOP = array (
 'disabled'          => 'Disabled',
 'featured'          => 'Featured',
 'taxable'           => 'Taxable',
+'nontaxable'        => 'Non-Taxable',
 'delete'            => 'Delete',
 'thanks_title'      => 'Thank you for your order!',
 'yes'               => 'Yes',
@@ -348,6 +349,10 @@ $LANG_SHOP = array (
     'reorder' => array(
         'name' => 'Item Reorder Report',
         'dscp' => 'List all items at or below their minimum stock levels.',
+    ),
+    'tax' => array(
+        'name' => 'Sales Tax Report',
+        'dscp' => 'List orders including tax and region information',
     ),
 ),
 'my_orders' => 'My Orders',
