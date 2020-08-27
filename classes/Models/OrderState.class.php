68f3bdc67f585ac4f1563343fc5e7c60e969f24c
Lee Garner <lee@leegarner.com>
2020-08-27 13:08:37 -0700

    Centralize order states with OrderState model

---
+++ classes/Models/OrderState.class.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Define order states.
+ *
+ * @author      Lee Garner <lee@leegarner.com>
+ * @copyright   Copyright (c) 2020 Lee Garner <lee@leegarner.com>
+ * @package     shop
+ * @version     v1.3.0
+ * @since       v1.3.0
+ * @license     http://opensource.org/licenses/gpl-2.0.php
+ *              GNU Public License v2 or later
+ * @filesource
+ */
+namespace Shop\Models;
+
+/**
+ * The state of the order.
+ * @package shop
+ */
+class OrderState
+{
+    /** Order is still in the shopping-cart phase.
+     */
+    public const CART = 'cart';
+
+    /** Indicates the order is open. Open orders may be updated.
+     */
+    public const PENDING = 'pending';
+
+    /** Order has been invoiced and is awaiting payment.
+     */
+    public const INVOICED = 'invoiced';
+
+    /** Order is in-process.
+     */
+    public const PROCESSING = 'processing';
+
+    /** Order has been shipped complete.
+     */
+    public const SHIPPED = 'shipped';
+
+    /** Order has been refunded.
+     */
+    public const REFUNDED = 'refunded';
+
+    /** Order is complete and paid. No further action needed.
+     */
+    public const CLOSED = 'closed';
+}
