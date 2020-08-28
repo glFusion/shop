35fe6d46455e08bcb6c0eafba3786e4060843db8
Lee Garner <lee@leegarner.com>
2020-08-28 07:36:10 -0700

    Comment update

--- upgrade.inc.php
+++ upgrade.inc.php
@@ -527,7 +527,6 @@ function SHOP_remove_old_files()
             'templates/attribute_form.thtml',
             // 1.3.0
             'vendor/square/connect',
-            // 1.3.1
             'Autoload.class.php',
         ),
         // public_html/shop
