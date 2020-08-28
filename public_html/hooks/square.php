f757e04a27032f6cff800e5d8969bafd06d89e78
Lee Garner <lee@leegarner.com>
2020-08-28 10:07:51 -0700

    Remove extra parentheses

--- public_html/hooks/square.php
+++ public_html/hooks/square.php
@@ -18,7 +18,7 @@ require_once '../../lib-common.php';
 // Get the complete IPN message prior to any processing
 SHOP_log("Recieved Hook:", SHOP_LOG_DEBUG);
 $json = file_get_contents('php://input');
-SHOP_log(("INPUT: " . $json, SHOP_LOG_DEBUG);
+SHOP_log("INPUT: $json", SHOP_LOG_DEBUG);
 /*COM_errorLog("POST: " . var_export($_POST,true));
 COM_errorLog("GET: " . var_export($_GET,true));
 COM_errorLog("HEADERS: " . var_export($_SERVER,true));*/
