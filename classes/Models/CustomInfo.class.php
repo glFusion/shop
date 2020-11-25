4564c8206608afd5df058718648fde9f8074102c
Lee Garner <lee@leegarner.com>
2020-11-25 14:52:40 -0800

    PHPDoc comments

--- classes/Models/CustomInfo.class.php
+++ classes/Models/CustomInfo.class.php
@@ -20,8 +20,15 @@ namespace Shop\Models;
  */
 class CustomInfo implements \ArrayAccess
 {
+    /** Information properties.
+     * @var array */
     private $properties = array();
 
+
+    /** Initialize the properties from a supplied string or array.
+     *
+     * @param   string|array    $val    Optonal initial properties
+     */
     public function __construct($val='')
     {
         if (is_string($val) && !empty($val)) {
@@ -35,64 +42,115 @@ class CustomInfo implements \ArrayAccess
     }
 
 
+    /**
+     * Set a property when accessing as an array.
+     *
+     * @param   string  $key    Property name
+     * @param   mixed   $value  Property value
+     */
     public function offsetSet($key, $value)
     {
         $this->properties[$key] = $value;
     }
 
+
+    /**
+     * Check if a property is set when calling `isset($this)`.
+     *
+     * @param   string  $key    Property name
+     * @return  boolean     True if property exists, False if not
+     */
     public function offsetExists($key)
     {
         return isset($this->properties[$key]);
     }
 
+
+    /**
+     * Remove a property when using unset().
+     *
+     * @param   string  $key    Property name
+     */
     public function offsetUnset($key)
     {
         unset($this->properties[$key]);
     }
 
+
+    /**
+     * Get a property when referencing the class as an array.
+     *
+     * @param   string  $key    Property name
+     * @return  mixed       Property value, NULL if not set
+     */
     public function offsetGet($key)
     {
         return isset($this->properties[$key]) ? $this->properties[$key] : NULL;
     }
 
+
+    /**
+     * Check if the properties array is empty.
+     *
+     * @return  boolean     True if empty, False if not
+     */
     public function isEmpty()
     {
         return empty($this->properties);
     }
 
 
+    /**
+     * Merge the supplied array into the internal properties.
+     *
+     * @param   array   $arr    Array of data to add
+     * @return  object  $this
+     */
     public function merge(array $arr)
     {
         $this->properties = array_merge($this->properties, $arr);
         return $this;
     }
 
+
+    /**
+     * Get the internal properties as a native array.
+     *
+     * @return  array   $this->properties
+     */
     public function toArray()
     {
         return $this->properties;
     }
 
-    public function serialize()
-    {
-        return serialize($this->properties);
-    }
 
-    public function unserialize($data)
-    {
-        $this->properties = @unserialize($data);
-    }
-
-
+    /**
+     * Encode the internal properties to a Base64-encode JSON string.
+     *
+     * @return  string      Encoded string containing the properties
+     */
     public function encode()
     {
         return base64_encode(json_encode($this->properties));
     }
 
+
+    /**
+     * Sets the internal properties by decoding the supplied string.
+     *
+     * @param   string  $data   Base64-encoded JSON string
+     */
     public function decode($data)
     {
         $this->properties = json_decode(base64_decode($data), true);
     }
 
+
+    /**
+     * Return the string representation of the class.
+     *
+     * @return  string      Base64-encoded json string
+     */
     public function __toString()
     {
         return $this->encode();
