c2f2a000c7f31472a04768e84a4e891bbe2351da
Lee Garner <lee@leegarner.com>
2021-01-08 07:54:09 -0800

    Cleanup webhook and ipn endpoints

--- public_html/ipn/square.php
+++ public_html/ipn/square.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * IPN processor for Square notifications.
+ * Webhook handler for Square. Included as IPN endpoint for compatibility
  *
  * @author      Lee Garner <lee@leegarner.com>
- * @copyright   Copyright (c) 2018-2019 Lee Garner <lee@leegarner.com>
+ * @copyright   Copyright (c) 2020 Lee Garner <lee@leegarner.com>
  * @package     shop
- * @version     v0.7.0
- * @since       v0.7.0
+ * @version     v1.3.0
+ * @since       v1.3.0
  * @license     http://opensource.org/licenses/gpl-2.0.php
  *              GNU Public License v2 or later
  * @filesource
@@ -16,14 +16,15 @@
 require_once '../../lib-common.php';
 
 // Get the complete IPN message prior to any processing
-SHOP_log('Recieved Square IPN: ' . var_export($_GET, true), SHOP_LOG_DEBUG);
-
-// Process IPN request
-$ipn = \Shop\IPN::getInstance('square', $_GET);
-if ($ipn->Process()) {
-    echo COM_refresh(SHOP_URL . '/index.php?thanks=square');
-} else {
-    echo COM_refresh(SHOP_URL . '/index.php?msg=8');
+SHOP_log("Recieved Hook:", SHOP_LOG_DEBUG);
+$json = file_get_contents('php://input');
+SHOP_log("INPUT: $json", SHOP_LOG_DEBUG);
+/*COM_errorLog("POST: " . var_export($_POST,true));
+COM_errorLog("GET: " . var_export($_GET,true));
+COM_errorLog("HEADERS: " . var_export($_SERVER,true));*/
+$WH = Shop\Webhook::getInstance('square');
+if ($WH->Verify()) {
+    $WH->Dispatch();
 }
-
+exit;
 ?>
--- public_html/ipn/stripe.php
+++ public_html/ipn/stripe.php
@@ -1,25 +1,31 @@
 <?php
 /**
  * IPN processor for Stripe notifications.
+ * Deprecated as of v1.3.0, included here for backwards compatibility.
  *
  * @author      Lee Garner <lee@leegarner.com>
  * @copyright   Copyright (c) 2019 Lee Garner <lee@leegarner.com>
  * @package     shop
- * @version     v1.1.0
+ * @version     v1.3.0
  * @since       v0.7.1
  * @license     http://opensource.org/licenses/gpl-2.0.php
  *              GNU Public License v2 or later
  * @filesource
+ * @deprecated  v1.3.0
  */
 
 require_once '../../lib-common.php';
 
-$ipn = \Shop\IPN::getInstance('stripe');
-if ($ipn) {
-    $ipn->Process();
-} else {
-    SHOP_log('Stripe IPN processor not found');
+$gw_name = SHOP_getVar($_GET, '_gw');
+if (!empty($gw_name)) {
+    $WH = Shop\Webhook::getInstance($gw_name);
+    if ($WH && $WH->Verify()) {
+        $WH->Dispatch();
+    } else {
+        SHOP_log("Webhook verification failed for $gw_name");
+    }
 }
+
 http_response_code(200);
 
 ?>
