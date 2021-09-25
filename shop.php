b720e2a7adfa03aa6e65adffaca2ca8d19670ade
Lee Garner <lee@leegarner.com>
2021-09-25 10:52:22 -0700

    Bump release version

--- shop.php
+++ shop.php
@@ -20,7 +20,7 @@ if (!defined ('GVERSION')) {
 
 global $_DB_table_prefix, $_TABLES;
 
-Shop\Config::set('pi_version', '1.3.1');
+Shop\Config::set('pi_version', '1.4.0');
 Shop\Config::set('gl_version', '1.7.9');
 
 $_SHOP_table_prefix = $_DB_table_prefix . 'shop_';
