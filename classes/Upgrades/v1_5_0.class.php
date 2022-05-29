8c8601f5948b95d61f826af419e85a23a6233e80
Lee Garner <lee@leegarner.com>
2022-05-29 16:02:43 -0700

    Update ipnlog ref_id when column is added to DB

--- classes/Upgrades/v1_5_0.class.php
+++ classes/Upgrades/v1_5_0.class.php
@@ -14,6 +14,8 @@ namespace Shop\Upgrades;
 use Shop\Config;
 use Shop\OrderItem;
 use Shop\ProductVariant;
+use glFusion\Database\Database;
+use glFusion\Log\Log;
 
 
 class v1_5_0 extends Upgrade
@@ -30,9 +32,53 @@ class v1_5_0 extends Upgrade
             $c = \config::get_instance();
             $c->set('shop_email', $admin_mail, Config::PI_NAME);
         }
+
+        // Update the payment ref_id column in the IPN log.
+        $upd_ref_id = !self::tableHasColumn('shop.ipnlog', 'ref_id');
+        if ($upd_ref_id) {
+            // For gateways that have only one webhook sent, do it the easy way.
+            // Update the more complicated gateways after the column is added.
+            $SHOP_UPGRADE[self::$ver][] = "UPDATE {$_TABLES['shop.ipnlog']}
+                SET ref_id = txn_id WHERE gateway IN
+                ('paypal', 'test', 'check', 'coingate', 'authorizenet', 'paylike');";
+        }
         if (!self::doUpgradeSql(self::$ver, self::$dvlp)) {
             return false;
         }
+
+        // Now update gateways where the payment ID is different than the Webhook ID.
+        $db = Database::getInstance();
+        if ($upd_ref_id) {
+            try {
+                $ipns = $db->conn->executeQuery(
+                    "SELECT id, gateway, ipn_data, txn_id
+                    FROM {$_TABLES['shop.ipnlog']}
+                    WHERE gateway IN ('stripe', 'square', 'ppcheckout')"
+                )->fetchAllAssociative();
+            } catch (\Exception $e) {
+                Log::write('system', Log::ERROR, __METHOD__ . ': ' . $e->getMessage());
+                $ipns = false;
+            }
+            if (!empty($ipns)) {
+                foreach ($ipns as $ipn) {
+                    $ref_id = self::getRefFromIpnJson($ipn);
+                    if (!empty($ref_id)) {
+                        try {
+                            $db->conn->executeStatement(
+                                "UPDATE {$_TABLES['shop.ipnlog']}
+                                SET ref_id = ? WHERE id = ?",
+                                array($ref_id, $ipn['id']),
+                                array(Database::STRING, Database::INTEGER)
+                            );
+                        } catch (\Exception $e) {
+                            // Log the error but no reason to stop
+                            Log::write('system', Log::ERROR, __METHOD__ . ': ' . $e->getMessage());
+                        }
+                    }
+                }
+            }
+        }
+
         $sql = "SELECT * FROM {$_TABLES['shop.orderitems']}";
         $res = DB_query($sql);
         if ($res) {
@@ -53,6 +99,39 @@ class v1_5_0 extends Upgrade
             }
         }
         return self::setVersion(self::$ver);
+    }
+
+
+    /**
+     * Get the payment reference ID from the IPN data.
+     * This could be handled by each gateway, but some are installable modules
+     * and the latest version may not be available.
+     *
+     * @param   array   $A  Record array from the ipnlog table
+     * @return  string      Payment ref_id, empty string if unable to decode.
+     */
+    public static function getRefFromIPNJson(array $A) : string
+    {
+        $obj = json_decode($A['ipn_data']);
+        $retval = '';
+        switch ($A['gateway']) {
+        case 'ppcheckout':
+            if (isset($obj->resource->id)) {
+                $retval = $obj->resource->id;
+            }
+            break;
+        case 'stripe':
+            if (isset($obj->data->object->payment_intent)) {
+                $retval = $obj->data->object->payment_intent;
+            }
+            break;
+        case 'square':
+            if (isset($obj->data->id)) {
+                $retval = $obj->data->id;
+            }
+            break;
+        }
+        return $retval;
     }
 
 }
--- sql/mysql_install.php
+++ sql/mysql_install.php
@@ -967,7 +967,7 @@ $SHOP_UPGRADE['1.5.0'] = array(
     "ALTER TABLE {$_TABLES['shop.orderitems']} DROP `txn_type`",
     "ALTER TABLE {$_TABLES['shop.orderitems']} ADD `shipping_weight` decimal(9,4) unsigned NOT NULL DEFAULT 0 AFTER `shipping_units`",
     "DROP TABLE {$_TABLES['shop.cache']}",
-    "ALTER TABLE {$_TABLES['shop.ipnlog']} ADD `ref_id` varchar(127) NOT NULL DEFAULT ''",
+    "ALTER TABLE {$_TABLES['shop.ipnlog']} ADD `ref_id` varchar(127) NOT NULL DEFAULT '' AFTER `txn_id`",
     "ALTER TABLE {$_TABLES['shop.ipnlog']} ADD KEY `gw_ref` (`gateway`,`ref_id`)",
 );
 
