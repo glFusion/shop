4b54b2f4fdaff542071f3e9524d5fa3deb056ee6
Lee Garner <lee@leegarner.com>
2023-05-24 11:10:34 -0700

    Update Request class to handle friendly URLs

--- classes/Models/Request.class.php
+++ classes/Models/Request.class.php
@@ -5,7 +5,7 @@
  * parameters are "stuffed" into the parameter array.
  *
  * @author      Lee Garner <lee@leegarner.com>
- * @copyright   Copyright (c) 2022 Lee Garner <lee@leegarner.com>
+ * @copyright   Copyright (c) 2022-2023 Lee Garner <lee@leegarner.com>
  * @package     shop
  * @version     v1.5.0
  * @since       v1.5.0
@@ -32,6 +32,87 @@ class Request extends DataArray
     public function __construct(?array $A=NULL)
     {
         $this->properties = array_merge($_GET, $_POST);
+    }
+
+
+    /**
+     * Get arguments from friendly URLs.
+     * Expands urls like '/index.php/var1/var2/var3.... to an array of
+     * name->value using the supplied names.
+     *
+     * If there are more arguments than names the remaining args are named `argX`.
+     * If there are more names than args then the names are added with empty
+     * values.
+     *
+     * @param   array   $names  Array of argument names.
+     * @return  object  $this
+     */
+    public function withArgNames(array $names) : self
+    {
+        $args = array();
+        if (isset ($_SERVER['PATH_INFO'])) {
+            if ($_SERVER['PATH_INFO'] == '') {
+                if (isset ($_ENV['ORIG_PATH_INFO'])) {
+                    $arguments = explode('/', $_ENV['ORIG_PATH_INFO']);
+                } else {
+                    $arguments = array();
+                }
+            } else {
+                $arguments = explode('/', $_SERVER['PATH_INFO']);
+            }
+            array_shift ($arguments);
+            if (isset($arguments[0]) && $arguments[0] == substr($_SERVER['SCRIPT_NAME'], 1)) {
+                array_shift($arguments);
+            }
+        } elseif (isset($_ENV['ORIG_PATH_INFO'])) {
+            $scriptName = array();
+            $scriptName = explode('/', $_SERVER['SCRIPT_NAME']);
+            array_shift($scriptName);
+            $arguments = explode('/', substr($_ENV['ORIG_PATH_INFO'], 1));
+            if ($arguments[0] == $scriptName[0]) {
+                $arguments = array();
+            }
+        } elseif (isset($_SERVER['ORIG_PATH_INFO'])) {
+            $arguments = explode('/', substr($_SERVER['ORIG_PATH_INFO'], 1));
+            array_shift ($arguments);
+            $script_name = strrchr($_SERVER['SCRIPT_NAME'], '/');
+            if ($script_name == '') {
+                $script_name = $_SERVER['SCRIPT_NAME'];
+            }
+            $indexArray = 0;
+            $search_script_name = substr($script_name, 1);
+            if (array_search($search_script_name, $arguments) !== false) {
+                $indexArray = array_search($search_script_name,$arguments);
+            }
+            for ($x=0; $x < $indexArray; $x++) {
+                array_shift($arguments);
+            }
+            if (isset($arguments[0]) && $arguments[0] == substr($script_name, 1)) {
+                array_shift($arguments);
+            }
+        } else {
+            $arguments = array ();
+        }
+
+        $namecount = count($names);
+        $argcount = count($arguments);
+        for ($i = 0; $i < $namecount && $i < $argcount; $i++) {
+            $args[$names[$i]] = $arguments[$i];
+        }
+        if ($i >= $namecount) {     // got to the end of names, add more args
+            for (; $i < $argcount; $i++) {
+                $args['arg' . $i+1] = $arguments[$i];
+            }
+        } elseif ($i >= $argcount) {    // got to end of args, add empty names
+            for (; $i < $namecount; $i++) {
+                $args[$names[$i]] = '';
+            }
+        }
+
+        if (!empty($args)) {
+            $this->properties = array_merge($args, $this->properties);
+        }
+        return $this;
     }
 
 
@@ -94,5 +175,37 @@ class Request extends DataArray
         return http_build_query($this->properties);
     }
 
-}
 
+    /**
+    * Builds crawler friendly URL if URL rewriting is enabled.
+    *
+    * This function will attempt to build a crawler friendly URL. If this feature is
+    * disabled because of platform issue it just returns original $url value.
+    *
+    * https://site.com/index.php?a=1&b=2 becomes https://site.com/index.php/1/2
+    *
+    * @param    string  $url    URL to try and convert
+    * @return   string      Rewritten if _isenabled is true otherwise original url
+    */
+    public function buildUrl(string $url) : string
+    {
+        global $_CONF;
+
+        // Do nothing if not configured to rewrite URLs
+        if (!$_CONF['url_rewrite']) {
+            return $url;
+        }
+
+        // Get the URL parts and parse any query string into an array
+        $parts = parse_url($url);
+        if (!isset($parts['query']) || empty($parts['query'])) {
+            // No rewrite needed if no query string
+            return $url;
+        }
+        parse_str($parts['query'], $qparts);
+        $str = implode('/', $qparts);
+        $retval = $parts['scheme']. '://' . $parts['host'] . $parts['path'] . '/' . $str;
+        return COM_sanitizeURL($retval);
+    }
+
+}
